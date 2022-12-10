c16b216b0ab106d5d6d83e0d0ab415cbe6555c35
Carl Bennett <carl@sharesies.co.nz>
2022-12-11 12:39:58 +1300

    Day 10 one-liners

--- 10.js
+++ 10.js
@@ -31,3 +31,5 @@ for (let l = 0; l < 240 / 40; l++) {
     console.log(crt.slice(l*40, l*40+40).join(''))
 }
 
+console.log(input.trim().split('\n').flatMap(line => line === "noop" ? [line] : ['noop', line]).reduce(([total, register], line, cycle) => [(cycle + 21) % 40 ? total : total + (cycle + 1) * register, line.startsWith('addx') ? register + 1*line.split(' ')[1] : register], [0, 1])[0])
+console.log(input.trim().split('\n').flatMap(line => line === "noop" ? [line] : ['noop', line]).reduce(([crt, register], line, cycle) => [Object.values({...crt, [cycle]: Math.abs(register - cycle % 40) <= 1 ? '#' : '.'}), line.startsWith('addx') ? register + 1*line.split(' ')[1] : register], [Array(240).fill('.'), 1])[0].reduce((output, pixel, i) => i % 40 ? output + pixel : output + '\n' + pixel, ''))
