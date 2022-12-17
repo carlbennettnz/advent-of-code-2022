8f4ec86186c9cc845307b732d42a921b899a325a
Carl Bennett <carl@sharesies.co.nz>
2022-12-18 11:35:25 +1300

    17 working part 2

--- 17.py
+++ 17.py
@@ -1,18 +1,18 @@
+from collections import defaultdict
 from typing import NamedTuple
-import rich
 
-jets = list(open('17.txt', 'r').read())
+jets = [-1 if jet == "<" else 1 for jet in open('17.txt', 'r').read()]
 num_rocks = 1000000000000
 
 rocks = [
     [[True] * 4],
-    [[False, True, False], [True, False, True], [False, True, False]],
+    [[False, True, False], [True] * 3, [False, True, False]],
     [[False, False, True], [False, False, True], [True] * 3],
     [[True]] * 4,
     [[True] * 2] * 2
 ]
 
-cave = set()
+tower = defaultdict(list)
 tick = 0
 
 class Point(NamedTuple):
@@ -33,71 +33,92 @@ def points_in_rock(rock, location):
     return points
 
 def collides(rock, location):
+    if location.x < 0 or location.x + len(rock[0]) > 7 or location.y - len(rock) + 1 < 0:
+        return True
+
     for point in points_in_rock(rock, location):
-        if point in cave or point.x < 0 or point.x >= 7 or point.y < 0:
+        if point.x in tower[point.y]:
             return True
 
     return False
 
 def print_cave(rock_points = None):
     if not rock_points:
-        rock_points = cave
-    top = max(p.y for p in rock_points)
-    for y in range(top, max(top - 30, 0), -1):
-        print(''.join('#' if Point(x, y) in cave else ('@' if Point(x, y) in rock_points else '.') for x in range(0, 7)))
+        rock_points = []
+
+    top = max(p.y for p in rock_points) if rock_points else max(tower.keys())
+
+    for y in range(top, max(top - 30, 0) - 1, -1):
+        print(''.join('#' if x in tower[y] else ('@' if Point(x, y) in rock_points else '.') for x in range(0, 7)))
 
 max_height = 0
 last_max_height = 0
 deltas = []
-cycle_size = len(jets) * len(rocks)
-print(num_rocks / cycle_size)
+cycle_size = len(rocks)
+stop_after = None
+projection = None
 
-# for rock_num in range(0, len(jets) * len(rocks) * 100):
 for rock_num in range(0, num_rocks):
     rock = rocks[rock_num % len(rocks)]
-    location = Point(2, max(p.y for p in cave | {Point(0, -1)}) + 3 + len(rock))
-    starting_tick = tick
+    max_y_in_tower = max([y for y in tower if tower[y]]) if rock_num else -1
+    location = Point(2, max_y_in_tower + 3 + len(rock))
 
+    # Position the rock on the tower
     while True:
-        dx = -1 if jets[tick % len(jets)] == "<" else 1
-        new_location = Point(location.x + dx, location.y)
+        new_location = Point(location.x + jets[tick % len(jets)], location.y)
+        tick += 1
+
         if not collides(rock, new_location):
             location = new_location
 
         new_location = Point(location.x, location.y - 1)
-        tick += 1
 
-        if tick - starting_tick > 3 and collides(rock, new_location):
+        if collides(rock, new_location):
             break
 
         location = new_location
 
-    cave -= {p for p in cave if p.y < location.y - 100}
-    cave |= {*points_in_rock(rock, location)}
+    # Add rock to the tower
+    for point in points_in_rock(rock, location):
+        tower[point.y].append(point.x)
 
+    # Forget about parts of the stack more than 100 layers deep. An ultimately-unnecessary optimisation.
+    for y in [y for y in tower if y < location.y - 100]:
+        del tower[y]
+
+    if rock_num + 1 == 2022:
+        print('part 1:', max(y for y in tower if tower[y]) + 1)
+
+    # If we've completed a cycle, check for repeating patterns in the delta of each cycle. If we find that the
+    # last, say, 50 deltas were the same as the 50 before that, and those are the same as the 50 before, we
+    # assume the pattern will repeat forever. We can extrapolate and print the answer.
     if rock_num and rock_num % cycle_size == 0:
-        last_max_height = max_height
-        max_height = max(p.y for p in cave)
+        last_max_height, max_height = max_height, max(y for y in tower if tower[y])
         delta = max_height - last_max_height
         deltas.append(delta)
-        rich.print(delta, max_height, num_rocks // rock_num * max_height)
 
         for n in range(2, len(deltas) // 2):
+            # Check if the last 3 sets of n cycles match
             if deltas[-n:] == deltas[-2*n:-n] == deltas[-3*n:-2*n]:
+                # There are now 4 different numbers we need to get the answer:
+                # - The height so far
+                # - The size of all the repeating sets of n cycles we can fit between now and the final rock
+                # - The size of a partial set of cycles at the end
+                # - The size of a partial cycle after that
+                # We'll need to drop a few more rocks to get the last number.
                 repeat_size = sum(deltas[-n:])
                 rocks_remaining = num_rocks - rock_num
-                repeats_remaining = int(rocks_remaining / cycle_size / n)
-                extra_cycles = rocks_remaining % (cycle_size * n)
-                additional_cycles = deltas[-n:-n + extra_cycles // cycle_size]
-                missed = rocks_remaining % cycle_size
-                print(additional_cycles)
-                projection = max_height + repeats_remaining * repeat_size + sum(additional_cycles)
-                print('expected ', 1514285714288)
-                print('projected', projection)
-                print('repeats start after', rock_num // cycle_size - 3 * n, 'cycles')
-                print('missed', missed)
-                exit()
-        # print_cave({location})
+                repeats_remaining = rocks_remaining // (cycle_size * n)
+                rocks_after_last_repeat = rocks_remaining % (cycle_size * n)
+                delta_of_partial_repeat = deltas[-n:-n + rocks_after_last_repeat // cycle_size]
+                beyond_last_cycle = rocks_remaining % cycle_size
+                projection = max_height + repeats_remaining * repeat_size + sum(delta_of_partial_repeat)
+                # print(f'repeats start after {rock_num // cycle_size - 3 * n} cycles')
+                # print(f'there are {beyond_last_cycle} rocks beyond the last cycle')
+                stop_after = rock_num + beyond_last_cycle
 
-print_cave()
-print(max(p.y for p in cave) + 1)
+    if rock_num == stop_after:
+        delta = max(y for y in tower if tower[y]) - max_height
+        projection += delta
+        print('part 2:', projection)
+        exit()
