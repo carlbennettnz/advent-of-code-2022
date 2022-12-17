44ec71aee725c88520ebe13b328515624b187922
Carl Bennett <carl@sharesies.co.nz>
2022-12-17 21:21:26 +1300

    17 part 2 sample

--- 17.py
+++ 17.py
@@ -1,13 +1,12 @@
 from typing import NamedTuple
-
 import rich
 
-jets = list(open('17s.txt', 'r').read())
+jets = list(open('17.txt', 'r').read())
 num_rocks = 1000000000000
 
 rocks = [
     [[True] * 4],
-    [[False, True, False], [True] * 3, [False, True, False]],
+    [[False, True, False], [True, False, True], [False, True, False]],
     [[False, False, True], [False, False, True], [True] * 3],
     [[True]] * 4,
     [[True] * 2] * 2
@@ -47,15 +46,17 @@ def print_cave(rock_points = None):
     for y in range(top, max(top - 30, 0), -1):
         print(''.join('#' if Point(x, y) in cave else ('@' if Point(x, y) in rock_points else '.') for x in range(0, 7)))
 
-formations = set()
 max_height = 0
 last_max_height = 0
 deltas = []
+cycle_size = len(jets) * len(rocks)
+print(num_rocks / cycle_size)
 
 # for rock_num in range(0, len(jets) * len(rocks) * 100):
-for rock_num in range(0, 2022):
+for rock_num in range(0, num_rocks):
     rock = rocks[rock_num % len(rocks)]
     location = Point(2, max(p.y for p in cave | {Point(0, -1)}) + 3 + len(rock))
+    starting_tick = tick
 
     while True:
         dx = -1 if jets[tick % len(jets)] == "<" else 1
@@ -66,7 +67,7 @@ for rock_num in range(0, 2022):
         new_location = Point(location.x, location.y - 1)
         tick += 1
 
-        if collides(rock, new_location):
+        if tick - starting_tick > 3 and collides(rock, new_location):
             break
 
         location = new_location
@@ -74,22 +75,29 @@ for rock_num in range(0, 2022):
     cave -= {p for p in cave if p.y < location.y - 100}
     cave |= {*points_in_rock(rock, location)}
 
-    # if rock_num and rock_num % (len(jets) * len(rocks)) == 0:
-    #     # formations.add(tuple(sorted([Point(p.x, p.y - location.y) for p in cave], key=lambda p: p.y * 2^8 + p.x)))
-    #     last_max_height = max_height
-    #     max_height = max(p.y for p in cave)
-    #     deltas.append(max_height - last_max_height)
-    #     rich.print((max_height - last_max_height) % 79900)
-    #
-    #     for n in range(2, len(deltas) // 2):
-    #         if deltas[-n:] == deltas[-2*n:-n] == deltas[-3*n:-2*n]:
-    #             repeat_size = sum(deltas[-n:])
-    #             initial_size = sum(deltas[0:-3*n])
-    #             num_repeats = (num_rocks - (len(deltas) - 3 * n)) / (len(jets) * len(rocks))
-    #             print(num_repeats)
-    #             rich.print('found a repeat', dict(n=n, repeat_size=repeat_size, initial_size=initial_size, num_repeats=num_repeats, total=initial_size + repeat_size * num_repeats))
-    #             # exit()
-    #     # print_cave({location})
+    if rock_num and rock_num % cycle_size == 0:
+        last_max_height = max_height
+        max_height = max(p.y for p in cave)
+        delta = max_height - last_max_height
+        deltas.append(delta)
+        rich.print(delta, max_height, num_rocks // rock_num * max_height)
+
+        for n in range(2, len(deltas) // 2):
+            if deltas[-n:] == deltas[-2*n:-n] == deltas[-3*n:-2*n]:
+                repeat_size = sum(deltas[-n:])
+                rocks_remaining = num_rocks - rock_num
+                repeats_remaining = int(rocks_remaining / cycle_size / n)
+                extra_cycles = rocks_remaining % (cycle_size * n)
+                additional_cycles = deltas[-n:-n + extra_cycles // cycle_size]
+                missed = rocks_remaining % cycle_size
+                print(additional_cycles)
+                projection = max_height + repeats_remaining * repeat_size + sum(additional_cycles)
+                print('expected ', 1514285714288)
+                print('projected', projection)
+                print('repeats start after', rock_num // cycle_size - 3 * n, 'cycles')
+                print('missed', missed)
+                exit()
+        # print_cave({location})
 
 print_cave()
 print(max(p.y for p in cave) + 1)
