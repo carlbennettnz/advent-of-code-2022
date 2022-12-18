04c3ea0ebede0f3d8b88971ffe622b0237ec4455
Carl Bennett <carl@sharesies.co.nz>
2022-12-18 19:55:07 +1300

    18 tidy

--- py/18.py
+++ py/18.py
@@ -1,62 +1,43 @@
-import dataclasses
-import itertools
-from rich import print
-
 input = [[int(p) for p in line.split(",")] for line in open("../in/18.txt", "r").read().strip().split("\n")]
+droplets = {(x, y, z) for x, y, z in input}
 
 
-@dataclasses.dataclass(frozen=True)
-class Vector:
-    x: int
-    y: int
-    z: int
-
-    def adj(self):
-        return [
-            Vector(self.x + 1, self.y, self.z),
-            Vector(self.x - 1, self.y, self.z),
-            Vector(self.x, self.y + 1, self.z),
-            Vector(self.x, self.y - 1, self.z),
-            Vector(self.x, self.y, self.z + 1),
-            Vector(self.x, self.y, self.z - 1),
-        ]
+def adj(p):
+    x, y, z = p
+    return [
+        (x + 1, y, z),
+        (x - 1, y, z),
+        (x, y + 1, z),
+        (x, y - 1, z),
+        (x, y, z + 1),
+        (x, y, z - 1),
+    ]
 
 
-droplets = [Vector(x, y, z) for x, y, z in input]
-sides = set()
+print("part 1:", sum(1 for d in droplets for a in adj(d) if a not in droplets))
 
-count = 0
-
-for droplet in droplets:
-    for adj in droplet.adj():
-        if adj not in droplets:
-            sides.add((droplet, adj))
-
-print("part 1:", len(sides))
-
-bounds = (
-    (min(p.x for p in droplets), max(p.x for p in droplets)),
-    (min(p.y for p in droplets), max(p.y for p in droplets)),
-    (min(p.z for p in droplets), max(p.z for p in droplets)),
-)
-outside = {Vector(bounds[0][0] - 1, bounds[1][0] - 1, bounds[2][0] - 2)}
+limit = max(max(x, y, z) for x, y, z in droplets)
+outside = set()
+outside_count = 0
 
 while True:
-    for x in range(bounds[0][0] - 1, bounds[0][1] + 2):
-        for y in range(bounds[1][0] - 1, bounds[1][1] + 2):
-            for z in range(bounds[2][0] - 1, bounds[2][1] + 2):
-                p = Vector(x, y, z)
+    for x in range(-1, limit + 2):
+        for y in range(-1, limit + 2):
+            for z in range(-1, limit + 2):
+                p = (x, y, z)
 
                 if p in droplets:
                     continue
 
-                if any(adj in outside for adj in p.adj()):
+                if outside_count == 0 and any(a == -1 or a == limit + 1 for a in p):
                     outside.add(p)
 
-    count = 0
+                if any(a in outside for a in adj(p)):
+                    outside.add(p)
 
-    for wall, space in sides:
-        if space in outside:
-            count += 1
+    if outside_count == len(outside):
+        break
 
-    print("part 2:", count)
+    outside_count = len(outside)
+
+print("part 2:", sum(1 for d in droplets for a in adj(d) if a not in droplets and a in outside))
